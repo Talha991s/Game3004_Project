f680940af30c8625919c4eb341f83e8b31dd452f
JosephPaolo <jpmalibiran@gmail.com>
2021-01-30 03:20:35 -0500

    Added functions to toggle or set minimap visibility

--- Assets/_Scripts/Minimap/MinimapScr.cs
+++ Assets/_Scripts/Minimap/MinimapScr.cs
@@ -2,6 +2,7 @@
  *  Date Created: January 29, 2021
  *  Last Updated: January 30, 2021
  *  Usage: Drag and drop the prefab this script is attached to into the Canvas as a child. You can also just drop this prefab anywhere in the scene and it will automatically find its rightful position within Canvas.
+ *  Then use SetTargetPlayer() to add a player chracter object that the minimap camera will follow.
  *  Description: 
  */
 
@@ -13,19 +14,25 @@ using UnityEngine.UI;
 public class MinimapScr : MonoBehaviour
 {
     [Header("Manualset References")]
-    [SerializeField] private Transform targetPlayerRef;             //The player chracter that the minimap will follow and center on
+    [SerializeField] private Transform targetPlayerRef;             //The player chracter that the minimap will follow and center on.
     
     [Header("Preset References")]
-    [SerializeField] private GameObject canvasContainerRef;         //Drag and Drop canvas gameobject here
-    [SerializeField] private Transform miniMapCamContainerRef;      //Container object that holds the camera for the minimap view
+    [SerializeField] private GameObject canvasContainerRef;         //Drag and Drop canvas gameobject here.
+    [SerializeField] private GameObject minimapMaskRef;
+    [SerializeField] private Transform miniMapCamContainerRef;      //Container object that holds the camera for the minimap view.
+    [SerializeField] private Material playerMinimapMarkerRef;       //The visual marker of how a player chracter will appear on the minimap.
+    [SerializeField] private Material enemyMinimapMarkerRef;       //The visual marker of how a player chracter will appear on the minimap.
 
     [Header("Minimap Settings")]
     [SerializeField] private float camFollowSpeed = 10;
+    //[SerializeField] private float miniMapSize = 1;
     //[SerializeField] private bool bUseCircleMask;
+
+    
 
     private void Awake() 
     {
-        InsureCanvasExists();               //Insures that this object is within Canvas 
+        InsureCanvasExists();               //Insures that this object is within Canvas.
         ExtractCameraToRootHierarchy();     //Extract the Minimap Camera from within the prefab and unto the root of the scene hierarchy.
     }
 
@@ -34,18 +41,20 @@ public class MinimapScr : MonoBehaviour
         FollowTargetPlayerWithCam();        //Allows the minimap camera to follow the player movements. 
     }
 
-    //Insures that this object is within Canvas 
-    private void InsureCanvasExists() {
+    //Insures that this object is within Canvas.
+    private void InsureCanvasExists() 
+    {
 
-        //Check if this object is already in its proper place- as a child of Canvas object. Note: MinimapScr.cs is supposed to be attached to a prefab that is meant to go in the Canvas
+        //Check if this object is already in its proper place- as a child of Canvas object. Note: MinimapScr.cs is supposed to be attached to a prefab that is meant to go in the Canvas.
         if (this.transform.parent) 
         {
-            if (this.transform.parent.GetComponent<Canvas>()) {
+            if (this.transform.parent.GetComponent<Canvas>()) 
+            {
                 return;
             }
         }
 
-        //Check if Canvas exists with the name 'Canvas'
+        //Check if Canvas exists with the name 'Canvas'.
         if (GameObject.Find("Canvas")) 
         {
             if (GameObject.Find("Canvas").GetComponent<Canvas>()) 
@@ -57,7 +66,7 @@ public class MinimapScr : MonoBehaviour
             }
         }
 
-        //Check if Canvas exists with the name 'HUD'
+        //Check if Canvas exists with the name 'HUD'.
         if (GameObject.Find("HUD"))
         {
             if (GameObject.Find("HUD").GetComponent<Canvas>()) 
@@ -69,7 +78,7 @@ public class MinimapScr : MonoBehaviour
             }
         }
 
-        //If Canvas object cannot be found: build one
+        //If Canvas object cannot be found: build one.
         canvasContainerRef = new GameObject();
         canvasContainerRef.gameObject.AddComponent<Canvas>();
         canvasContainerRef.gameObject.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
@@ -91,7 +100,8 @@ public class MinimapScr : MonoBehaviour
 
         miniMapCamContainerRef.SetParent(null);
 
-        if (targetPlayerRef) {
+        if (targetPlayerRef) 
+        {
             miniMapCamContainerRef.position = new Vector3(targetPlayerRef.transform.position.x, targetPlayerRef.transform.position.y + 10, targetPlayerRef.transform.position.z);
         }
     }
@@ -113,5 +123,113 @@ public class MinimapScr : MonoBehaviour
 
         miniMapCamContainerRef.position = Vector3.MoveTowards(miniMapCamContainerRef.position, new Vector3(targetPlayerRef.position.x, targetPlayerRef.position.y + 10, targetPlayerRef.position.z), camFollowSpeed * Time.deltaTime);
     }
+
+    //Untested WIP
+    private void AddMinimapMarker(Transform targetObj, int markerType) 
+    {
+        GameObject minimapMarker; 
+
+        if (markerType <= 0) 
+        {
+            Debug.LogError("[Error] Invalid minimap marker type; Aborting operation...");
+            return;
+        }
+
+        minimapMarker = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        minimapMarker.name = "Minimap Marker";
+        minimapMarker.layer = LayerMask.NameToLayer("Minimap Marker");
+        minimapMarker.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        minimapMarker.GetComponent<MeshRenderer>().receiveShadows = false;
+        Destroy(minimapMarker.GetComponent<MeshCollider>());
+
+        switch (markerType) 
+        {
+            case 0:
+                return;
+            case 1:
+                if (playerMinimapMarkerRef) 
+                {
+                    minimapMarker.GetComponent<MeshRenderer>().material = playerMinimapMarkerRef;
+                }
+                break;
+            default:
+                return;
+        }
+
+    }
+
+    //Untested WIP
+    private void AddMinimapMarker(Transform targetObj, MinimapMarker markerType) 
+    {
+        GameObject minimapMarker; 
+
+        minimapMarker = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+    }
+
+    //Set new player character that the minimap cam will follow.
+    public void SetTargetPlayer(Transform insertPlayer) 
+    {
+        targetPlayerRef = insertPlayer;
+        miniMapCamContainerRef.position = new Vector3(targetPlayerRef.transform.position.x, targetPlayerRef.transform.position.y + 10, targetPlayerRef.transform.position.z);
+
+        AddMinimapMarker(targetPlayerRef, MinimapMarker.PLAYER);
+    }
+
+    //Toggle whether or not the minimap is visible
+    public void ToggleMiniMapVisibility() 
+    {
+        //If minimapMaskRef cannot be found, try to find it. If it still cannot be found return with error log.
+        if (!minimapMaskRef) 
+        {
+            if (this.transform.GetChild(0)) 
+            {
+                if (this.transform.GetChild(0).gameObject.GetComponent<Mask>()) 
+                {
+                    minimapMaskRef = this.transform.GetChild(0).gameObject;
+                }
+            }
+            if (!minimapMaskRef)
+            { 
+                Debug.LogError("[Error] Minimap mask reference missing! Aborting operation...");
+                return;
+            }
+        }
+
+        if (minimapMaskRef.activeSelf) 
+        {
+            minimapMaskRef.SetActive(false); //Turn off Minimap visual 
+        }
+        else 
+        {
+            minimapMaskRef.SetActive(true);  //Turn on Minimap visual
+        }
+    }
+
+    //Set whether or not the minimap is visible
+    public void SetMiniMapVisibility(bool set) 
+    {
+        //If minimapMaskRef cannot be found, try to find it. If it still cannot be found return with error log.
+        if (!minimapMaskRef) 
+        {
+            if (this.transform.GetChild(0)) 
+            {
+                if (this.transform.GetChild(0).gameObject.GetComponent<Mask>()) 
+                {
+                    minimapMaskRef = this.transform.GetChild(0).gameObject;
+                }
+            }
+            if (!minimapMaskRef)
+            { 
+                Debug.LogError("[Error] Minimap mask reference missing! Aborting operation...");
+                return;
+            }
+        }
+
+        //Set Minimap visual
+        minimapMaskRef.SetActive(set);
+    }
+
+
 
 }
