5e19c7b0b33aa02b072b399394aedc50d83ebe03
nightskin <angelastray@hotmail.com>
2021-01-30 09:21:42 -0500

    Shimron's 2nd commit
    fixed rotation issues
    renamed Hamp to Player
    fixed smoothing issues

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
     // joystic object
     public Joystick joystick;
-    public Vector2 sensitivity;
+    public float sensitivity;
    
     //New Addition in hopes of making the character turn pls help- Amber
     Vector3 move;
@@ -35,25 +35,29 @@ public class PlayerMovement : MonoBehaviour
         move = new Vector3(transform.localPosition.x, 0, transform.localPosition.y).normalized;
         //  ^   ^    ^
         //movement
-        if (joystick.Horizontal > sensitivity.x)
+        if (joystick.Horizontal > sensitivity)
         {
             transform.position += new Vector3(runVel * Time.deltaTime, 0, 0);
         }
-        else if (joystick.Horizontal < -sensitivity.x)
+        else if (joystick.Horizontal < -sensitivity)
         {
             transform.position -= new Vector3(runVel * Time.deltaTime, 0, 0);
         }
 
-        if (joystick.Vertical > sensitivity.y)
+        if (joystick.Vertical > sensitivity)
         {
             transform.position += new Vector3(0, 0, runVel * Time.deltaTime);
         }
-        else if (joystick.Vertical < -sensitivity.y)
+        else if (joystick.Vertical < -sensitivity)
         {
             transform.position -= new Vector3(0, 0, runVel * Time.deltaTime);
         }
         
-
+        if(joystick.Direction.magnitude > sensitivity)
+        {
+            float angle = Mathf.Atan2(joystick.Direction.x, joystick.Direction.y) *  Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, angle + 180, 0);
+        }
     }
 
 
