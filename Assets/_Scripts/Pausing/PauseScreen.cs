6aa0d5fa1bec78f8f82bbd953718966d3352a1b0
JosephPaolo <jpmalibiran@gmail.com>
2021-02-06 12:17:58 -0500

    working on HUD prefab

--- Assets/_Scripts/Pausing/PauseScreen.cs
+++ Assets/_Scripts/Pausing/PauseScreen.cs
@@ -1,12 +1,3 @@
-/*  Author: Joseph Malibiran
- *  Date Created: January 30, 2021
- *  Last Updated: January 30, 2021
- *  Description: A simple way of pausing the game opposed to more involved methods. 'Time.timeScale = 0' can be used as a way to pause the game; This is basically a wrapper for Time.timeScale. UI buttons and Update() should still work.
- *  "Except for realtimeSinceStartup and fixedDeltaTime, timeScale affects all the time and delta time measuring variables of the Time class... FixedUpdate functions will not be called when timeScale is set to zero."
- *  This means all movement should be multiplied by Time.deltaTime for the pause effect to work. If you don't want to use Time.deltaTime in a movement (eg. rotation) use SimplePausingScr.IsGamePaused() to check whether 
- *  or not the game is paused. Then, disable controls or movement if the function returns true.
- */
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
